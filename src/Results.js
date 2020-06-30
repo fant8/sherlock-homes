813eafd2864db1f35dd92c0ebd90e934cd328ac0
fant8 <fant8@mcmaster.ca>
2020-06-29 21:41:12 -0400

    Added api get request?

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,20 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { Redirect, Route, HashRouter, NavLink } from 'react-router-dom';
 
 function Results(props){
-
+    useEffect (() => {
+        fetchItems();
+    }, [])
     
+    const fetchItems = async() => {
+        const data = await fetch("https://www12.statcan.gc.ca/rest/census-recensement/CPR2016.json?lang=E&dguid=2016A000011124&topic=0&notes=0&stat=0");
+        console.log(data);
+    }
+
     return(
-        <h1>{props.location.state.value}</h1>
+        <div>
+            <h1>{props.location.state.value}</h1>
+        </div>
     );
 
 }
